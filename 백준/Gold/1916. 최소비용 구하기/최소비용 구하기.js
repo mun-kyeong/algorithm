094454594c144b1b2a99b3587bf6d1a0cc5678a0
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-09-05 11:31:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 392 ms, Memory: 65304 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.js
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.js
@@ -1,45 +1,61 @@
 const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [n, m] = [input[0], input[1]].map(Number);
-const graph = Array.from(Array(n + 1), () => new Map());
-const INF = 100000 * 1000;
+const n = Number(input[0]);
+const m = Number(input[1]);
+const array = input.slice(2, m + 2).map((arr) => arr.split(" "));
 
-for (let i = 2; i < m + 2; i++) {
-  let [from, to, cost] = input[i].split(" ").map(Number);
-  if (!graph[from].has(to) || graph[from].get(to) > cost) {
-    graph[from].set(to, cost); // 최소 비용으로 간선 저장
+const [start, end] = input
+  .slice(m + 2)
+  .toString()
+  .split(" ")
+  .map(Number);
+
+// const tree = Array.from(Array(n + 1), () => new Map());
+// // const INF = 100000 * 1000;
+
+// for (let i = 2; i < m + 2; i++) {
+//   let [from, to, cost] = input[i].split(" ").map(Number);
+//   if (!tree[from].has(to) || tree[from].get(to) > cost) {
+//     tree[from].set(to, cost); // 최소 비용으로 간선 저장
+//   }
+// }
+
+const tree = array.reduce((acc, curr) => {
+  const [s, e, c] = curr;
+  if (!acc[s]) acc[s] = new Map();
+  if (!acc[s].has(e) || acc[s].get(e) > Number(c)) {
+    acc[s].set(e, Number(c));
+  }
+  return acc;
+}, {});
+
+const queue = [[start, 0]];
+const dist = Array(n + 1).fill(Infinity);
+dist[start] = 0;
+
+while (queue.length) {
+  queue.sort((a, b) => a[1] - b[1]);
+  const [startNode, cost] = queue.shift();
+
+  if (dist[startNode] < cost) continue;
+
+  if (Number(startNode) === end) {
+    console.log(dist[startNode]);
+    break;
+  }
+
+  // 여기서 런타임 에러남!!  tree[startNode] << 이거 순회하려고 할 때 원래 없는 값이면 런타임 에러를 출력 ..
+  //
+  for (const city of tree[startNode] || new Map()) {
+    const [nextNode, cost] = city;
+
+    const newConst = Number(dist[startNode]) + Number(cost);
+    if (newConst < dist[nextNode]) {
+      dist[nextNode] = newConst;
+      queue.push([nextNode, newConst]);
+    }
   }
 }
 
-const [start, end] = input[m + 2].split(" ").map(Number);
-
-function dijkstra(start) {
-  const distance = Array(n + 1).fill(INF);
-  distance[start] = 0;
-
-  const queue = [];
-  queue.push([0, start]); // cost, node 저장
-
-  while (queue.length) {
-    queue.sort((a, b) => a[0] - b[0]); //비용을 오름차순으로 정렬
-    const [currCost, currNode] = queue.shift(); //최단비용 꺼냄
-
-    if (distance[currNode] < currCost) continue; //이미 처리된 경로는 패스
-
-    graph[currNode].forEach((nextCost, nextNode) => {
-      const totalCost = currCost + nextCost;
-
-      if (totalCost < distance[nextNode]) {
-        //최단비용 갱신
-        distance[nextNode] = totalCost;
-        queue.push([totalCost, nextNode]);
-      }
-    });
-  }
-  return distance;
-}
-
-const result = dijkstra(start);
-console.log(result[end]);
+// console.log(dist[end]);
