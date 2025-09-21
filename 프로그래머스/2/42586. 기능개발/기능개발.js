42efe2e93fdfc230bcd559aa0a230e4fc6d7155c
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-09-21 21:28:14 +0900

    [level 2] Title: 기능개발, Time: 6.12 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,17 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    let max =0;
+    let count = 0;
+    for(let i=0; i<progresses.length; i++){
+        const day = Math.ceil((100 - progresses[i]) / speeds[i])
+        console.log(day)
+        if(max >= day) count ++;
+        else {
+            count!==0 && answer.push(count)
+            max = day;
+            count = 1;
+        }
+    }
+    answer.push(count)
+    return answer;
+}
