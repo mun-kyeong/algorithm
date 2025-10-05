c7d81b46f1dbd44a8f4056e4d4dd6f2e792c104b
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2025-10-05 23:44:49 +0900

    [level 3] Title: 산 모양 타일링, Time: 48.59 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.js
@@ -0,0 +1,20 @@
+function solution(n, tops) {
+    const MOD = 10007;
+    let dp = Array.from({ length: n }, () => [0, 0]);
+
+    // 초기값
+    dp[0][0] = 1;
+    dp[0][1] = tops[0] ? 3 : 2;
+
+    for (let i = 1; i < n; i++) {
+        if (tops[i] === 0) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + 2 * dp[i - 1][1]) % MOD;
+        } else {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+            dp[i][1] = (2 * dp[i - 1][0] + 3 * dp[i - 1][1]) % MOD;
+        }
+    }
+
+    return (dp[n - 1][0] + dp[n - 1][1]) % MOD;
+}
