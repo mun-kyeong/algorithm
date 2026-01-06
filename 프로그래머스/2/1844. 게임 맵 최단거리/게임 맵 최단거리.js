62589aa9ffcb3ad003a5e2539d17ff7968eb9754
mun-kyeong <96461334+mun-kyeong@users.noreply.github.com>
2026-01-06 14:32:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.38 ms, Memory: 34.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,41 +1,45 @@
-function solution(maps) {
-    
-    let maxX = maps.length;
-    let maxY = maps[0].length;
-    let answer = (maxX+1) * (maxY+1);
-    let queue = [[0,0,1]]
-    let countStorage = [1]
-    let queIdx = 0;
-    const visited = Array.from({length : maxX},()=> new Array(maxY).fill(false))
-    
-    let chrck = 0
-    while(queue.length){
-        const [x,y,currCount] = queue.shift()
+let m, n;
 
-        if(x===maxX-1 && y===maxY-1) {
-            answer = Math.min(answer, currCount)
-            continue;
+function solution(maps) {
+    m = maps[0].length, n =maps.length ;
+    let queue = [];
+    let visited = Array.from({length : n}, ()=> 
+        Array.from({length : m}, ()=> false)
+    )
+    
+    queue.push([0,0,1]) // [x,y,count]
+    visited[0][0] = true; // [x,y]
+    
+    let answer = -1;
+    while(queue.length){
+        // console.log(queue)
+        const [x,y,count] = queue.shift(); // [x,y,count]
+        if(x ===m-1 && y ===n-1) answer = count;
+        // x+1, y
+        if(x+1 < m && !visited[y][x+1] && maps[y][x+1] === 1){
+            visited[y][x+1] = true;
+            queue.push([x+1, y, count+1])
+        }
+           
+        // x-1, y
+        if(x-1 >= 0 && !visited[y][x-1] && maps[y][x-1] === 1){
+            visited[y][x-1] = true;
+            queue.push([x-1, y, count+1])
+        }
+           
+        // x, y +1
+        
+        if(y+1 <n && !visited[y+1][x] && maps[y+1][x] === 1 ){
+            visited[y+1][x] = true;
+            queue.push([x, y+1, count+1])
         }
         
-        if(x+1 < maxX && maps[x+1][y] ===1 && !visited[x+1][y]) {
-            visited[x+1][y] = true;
-            queue.push([x+1, y, currCount+1]);
-        }
-        if(x-1 >= 0 && maps[x-1][y] ===1&& !visited[x-1][y]) {
-            visited[x-1][y] = true
-            queue.push([x-1, y,currCount+1])
-        }
-         if(y+1 < maxY && maps[x][y+1] ===1&&!visited[x][y+1]) {
-            visited[x][y+1] = true;
-            queue.push([x, y+1,currCount+1])
-        }
-        if(y-1 >= 0 && maps[x][y-1] ===1&&!visited[x][y-1]) {
-            visited[x][y-1] = true
-            queue.push([x, y-1,currCount+1])
+       // x, y -1
+        if(y-1 >= 0 && !visited[y-1][x] && maps[y-1][x] === 1){
+            visited[y-1][x] = true;
+            queue.push([x, y-1, count+1])
         }
     }
-
     
-    if(answer === (maxX+1) * (maxY+1)) return -1
     return answer;
 }
